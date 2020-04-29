3b75c89542f63901e8ef8b071ca9a71338a88aae
addytheherp <addytheherp@gmail.com>
2020-04-29 18:29:49 +0200

    artefact rejection

--- project-alpha_decoding/ade_artifactrej.m
+++ project-alpha_decoding/ade_artifactrej.m
@@ -1,24 +1,24 @@
-% Artifact rejection , runICA; Additya - 5th September,2019
+% Artifact rejection , runICA; 
+% Created by: Additya
 
 %%
 clear ; clc ; clearvars ;
 
 % Fieldtrip path
-fieldtrip_path                                 = '/project/3015039.04/fieldtrip-20190618';
+fieldtrip_path                         = '/project/3015039.04/fieldtrip-20190618';
 addpath(fieldtrip_path); ft_defaults ;
 
-% subject, session lists
-list_sub                                       = {'21'};
-sub_list                                       = {'sub021'};
-list_session                                   = {'aud','vis'};
-nsub                                           = 1 ;
-nses                                           = input ('Enter 1 for aud; 2 for vis:' );
+%% subject, session lists
+list_sub                               = {'21'};
+sub_list                               = {'sub021'};
+list_session                           = {'aud','vis'};
+nsub                                   = 1 ;
+nses                                   = input ('Enter 1 for aud; 2 for vis:' );
 
 %% First artifact rejection
 % loading downsampled data
 dir_data                               = ['../data/' sub_list{nsub} '/preprocessed/'];
 fname                                  = ([dir_data 'sub0' list_sub{nsub} '_downsampled_' list_session{nses} '.mat']) ;
-
 fprintf('Loading %s \n',fname);
 load(fname);
 
@@ -27,12 +27,11 @@ cfg                                    = [];
 cfg.method                             = 'summary';
 cfg.alim                               = 1e-12;
 first_reject                           = ft_rejectvisual(cfg,downsampled_data);
-
 dir_data                               = ['/project/3015039.04/data/sub0' list_sub{nsub} '/preprocessed/'];
 fname                                  = ([dir_data 'sub0' list_sub{nsub} '_firstreject_' list_session{nses} '.mat']) ;
 fprintf('saving %s \n',fname); save(fname,'first_reject', '-v7.3');
 
-clear first_reject ; clear dir_data ; clear fname ; clear cfg; clear downsampled_data ;
+clear first_reject dir_data fname cfg downsampled_data ;
 
 %% runica
 % loading first rejection data
@@ -44,7 +43,6 @@ cfg                                    = [];
 cfg.method                             = 'runica';
 cfg.channel                            = 'MEG';
 comps_runica                           = ft_componentanalysis(cfg,first_reject);
-
 dir_data                               = ['/project/3015039.04/data/sub0' list_sub{nsub} '/preprocessed/'];
 fname                                  = ([dir_data 'sub0' list_sub{nsub} '_compsica_' list_session{nses} '.mat']) ;
 fprintf('saving %s \n',fname); save(fname,'comps_runica', '-v7.3');
@@ -79,12 +77,11 @@ ft_topoplotIC(cfg, comps_runica)
 cfg                                    = [];
 cfg.component                          = comps;       % to be removed component(s)
 firstreject_postica                    = ft_rejectcomponent(cfg,comps_runica,first_reject) ;
-
 dir_data                               = ['/project/3015039.04/data/sub0' list_sub{nsub} '/preprocessed/'];
 fname                                  = ([dir_data 'sub0' list_sub{nsub} '_firstreject_postica_' list_session{nses} '.mat']) ;
 fprintf('saving %s \n',fname); save(fname,'firstreject_postica', '-v7.3'); clear fname ;
 
-%% second artifact rejection - Trial by trial (ALWAYS ADJUST megscale !!!!)
+%% second artifact rejection - trial by trial (ALWAYS ADJUST megscale !!!!)
 cfg                                            = [];
 cfg.channel                                    = 'MEG';
 cfg.viewmode                                   = 'vertical';
@@ -93,7 +90,6 @@ cfg.megscale                                   = 1.3;     % adjusts meg channels
 cfg.method                                     = 'trial' ;
 
 secondreject_postica                           = ft_rejectvisual(cfg,firstreject_postica);
-
 dir_data                                       = ['/project/3015039.04/data/sub0' list_sub{nsub} '/preprocessed/'];
 fname                                          = ([dir_data 'sub0' list_sub{nsub} '_secondreject_postica_' list_session{nses} '.mat']) ;
 fprintf('saving %s \n',fname);
